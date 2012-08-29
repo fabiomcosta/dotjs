70460aaa212dce70a6aed1f2c81289449c564d6d
Fábio M. Costa <fabiomcosta@gmail.com>
2012-08-29 15:19:36 -0300

    More appropriate way of evaluating the script.
    From http://api.jquery.com/jQuery.ajax/
    More appropriate way of evaluating a request to a script.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,9 +1,6 @@
 $.ajax({
   url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
+  dataType: 'script',
   error: function(){
     console.log('no dotjs server found at localhost:3131')
   }
